df1e9fa0a106d28b7d5b69cb18da3a174d0bdc07
mekty2012 <41375658+mekty2012@users.noreply.github.com>
2021-05-11 13:30:37 +0900

    Update tree.py
    Implement decoding prefix formula

--- tree.py
+++ tree.py
@@ -33,3 +33,19 @@ def tree_to_list(formula):
     return curr
   else:
     raise ValueError("This function expects valid formula")
+
+def list_to_tree(words):
+  first = words[0]
+  if first == "x":
+    return symbolic.x, words[1:]
+  elif first.isdigit():
+    return int(first), words[1:]
+  elif first in symbolic.unary_ops:
+    child, rest = list_to_tree(words[1:])
+    return symbolic.generate_unary_tree(first, child), rest
+  elif first in symbolic.binary_ops:
+    child1, rest1 = list_to_tree(words[1:])
+    child2, rest2 = list_to_tree(rest1)
+    return symbolic.generate_binary_tree(first, child1, child2), rest2
+  else:
+    raise ValueError("This function expects valid formula")
