8813e7d7b7c8a61670bc3f2b97f56110b961dd5c
mekty2012 <41375658+mekty2012@users.noreply.github.com>
2021-05-24 17:40:33 +0900

    Update test_tree.py
    Rewrite test

--- test_tree.py
+++ test_tree.py
@@ -5,13 +5,12 @@ import sympy
 import pytest
 
 def test_code():
-  for i in range(10):
-    orig = symbolic.fixed_init(15)
-    encoded = tree.tree_to_list(orig)
-    decoded, rest = tree.list_to_tree(encoded)
-    print(orig)
-    print(sympy.srepr(orig))
-    print(encoded)
-    print(decoded)
-    assert len(rest) == 0
-    assert sympy.simplify(orig - decoded, rational=True) == sympy.simplify(0)
+  orig = symbolic.fixed_init(15)
+  encoded = tree.tree_to_list(orig)
+  decoded, rest = tree.list_to_tree(encoded)
+  print(orig)
+  print(sympy.srepr(orig))
+  print(encoded)
+  print(decoded)
+  assert len(rest) == 0
+  assert tree.tree_to_list(orig) == tree.tree_to_list(decoded)
